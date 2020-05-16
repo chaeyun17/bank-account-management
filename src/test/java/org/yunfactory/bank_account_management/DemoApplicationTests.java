f2bf1d92617e1432521b1736031cedd4087331a7
Chaeyun Lee <hopefulyun@gmail.com>
2020-05-17 05:25:40 +0900

    Init Project

---
+++ src/test/java/org/yunfactory/bank_account_management/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yunfactory.bank_account_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
