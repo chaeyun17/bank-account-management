c73061bbe041e68e8ffb80960e64f87ebf9cf013
Chaeyun Lee <hopefulyun@gmail.com>
2020-06-06 16:42:22 +0900

    Add: frontend build settings

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'extends': 'plugin:vue/essential',
+  "parser": "vue-eslint-parser",
+  "parserOptions": {
+    "parser": "babel-eslint",
+    "ecmaVersion": 2017,
+    "sourceType": "module"
+  }
+}
