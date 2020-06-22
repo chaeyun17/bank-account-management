a1acf0b45839ccd12fb89f18e31d8302aedf75a1
Chaeyun Lee <hopefulyun@gmail.com>
2020-06-22 22:59:44 +0900

    수정: aws codedeploy 스크립트 실행

---
+++ scripts/start_server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/deploy/bank_account
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -f bank_account_management)
+
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/*.jar $REPOSITORY/jar/
+
+JAR_NAME=$(ls $REPOSITORY/jar/ |grep 'bank_account_management' | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/jar/$JAR_NAME &
